77ba048b1701aad7ecb76b94a61bdcd2dd6b5811
IvoMandev <ivomandev6@gmail.com>
2024-01-23 16:16:43 +0200

    Nasledqvane test

---
+++ 23.01.24(1)/23.01.24(1)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._01._24_1_
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+
+    }
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+    public class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("Weeping...");
+        }
+    }
+    public class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Meowing...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog a = new Dog();
+            a.Bark();
+            a.Eat();
+            Cat b = new Cat();
+            b.Eat();
+            b.Meow();
+            Puppy p = new Puppy();
+            p.Eat();
+            p.Bark();
+            p.Weep();
+        }
+    }
+}
