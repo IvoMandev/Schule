ceb889a87f9ab601ace2706e7117e72bd5bd342d
IvoMandev <ivomandev6@gmail.com>
2024-01-30 16:40:52 +0200

    ITriangle2 dopulnenie

--- ITriangle/ITriangle/Program.cs
+++ ITriangle/ITriangle/Program.cs
@@ -29,14 +29,35 @@ namespace ITriangle
         double ITriangle.Face()
         {
             double p = (this.a + this.b + this.c) / 2;
-            return Math.Sqrt(p * (p-this.a) * (p - this.b) * (p-this.c));
+            return Math.Sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
         }
     }
+    public class ITriangle2: ITriangle
+    {
+        double a;
+        double b;
+        double alpha;
+        public ITriangle2(double A, double B, double Alpha)
+        {
+            this.a = A;
+            this.b = B;
+            this.alpha = (Alpha * Math.PI) / 180;
+        }
+        double ITriangle.Face()
+        {
+            return (this.a * this.b * Math.Sin(alpha)) / 2;
+        }
+        double ITriangle.Perimeter()
+        {
+            double c = Math.Sqrt(this.a * this.a + this.b * this.b - 2 * this.a * this.b * Math.Cos(alpha));
+            return this.a + this.b + c;
+        }
+        }
     class Program
     {
         static void Main(string[] args)
         {
-            ITriangle a = new ITriangle1(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+            ITriangle a = new ITriangle2(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
             Console.WriteLine($"Perimeter: {a.Perimeter()}");
             Console.WriteLine($"Face: {a.Face()}");
         }
