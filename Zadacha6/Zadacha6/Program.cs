ee449fd44158eb2cfde32b497193a565dba1f9ae
IvoMandev <ivomandev6@gmail.com>
2024-02-19 10:24:41 +0200

    sortira imena po azbuchen red(5ta)

---
+++ Zadacha6/Zadacha6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader r = new StreamReader("names.txt");
+            List<string> a = new List<string>();
+            while (!r.EndOfStream) a.Add(r.ReadLine());
+            a.Sort();
+            r.Close();
+            StreamWriter m = new StreamWriter("new_file.txt");
+            for (int i = 0; i < a.Count(); i++) m.WriteLine(a[i]);
+            m.Close();
+        }
+    }
+}
