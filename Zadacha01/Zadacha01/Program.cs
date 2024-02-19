e0d43ddc9cd4bd0e341543b15d8e0258c78a4653
IvoMandev <ivomandev6@gmail.com>
2024-02-19 09:39:50 +0200

    Chetene ot fail

---
+++ Zadacha01/Zadacha01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadacha01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader r = new StreamReader("string_text.txt");
+            List<string> a = new List<string>();
+            while (!r.EndOfStream)
+            {
+                a.Add(r.ReadLine());
+            }
+            
+            for (int i = 1; i < a.Count(); i+=2)
+            {
+                Console.WriteLine(a[i]);
+            }
+            r.Close();
+        }
+    }
+}
