e7f4450bcf672260c6228f91ed3c438cb5493f1c
IvoMandev <ivomandev6@gmail.com>
2024-02-19 09:44:53 +0200

    Smesvane 2 faila

---
+++ Zadacha2/Zadacha2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadacha2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader r = new StreamReader("string_text.txt");
+            StreamReader b = new StreamReader("oneLine.txt");
+            StreamWriter a = new StreamWriter("new_file.txt");
+            a.Write(r.ReadToEnd() + b.ReadToEnd());
+            r.Close();
+            b.Close();
+            a.Close();
+        }
+    }
+}
