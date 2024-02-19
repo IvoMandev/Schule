0ccba991c8ebc01fd9b361dc5a1fc663b5df986c
IvoMandev <ivomandev6@gmail.com>
2024-02-19 10:30:06 +0200

    Removes odd lines

--- Zadacha6/Zadacha6/Program.cs
+++ Zadacha6/Zadacha6/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Zadacha6
 {
@@ -13,7 +14,14 @@ namespace Zadacha6
             StreamReader r = new StreamReader("names.txt");
             List<string> a = new List<string>();
             while (!r.EndOfStream) a.Add(r.ReadLine());
-            a.Sort();
+            for (int i = 0; i < a.Count(); i++)
+            {
+                if (i % 2 != 0)
+                {
+                    a.Remove(a[i]);
+                    i++;
+                }
+            }
             r.Close();
             StreamWriter m = new StreamWriter("new_file.txt");
             for (int i = 0; i < a.Count(); i++) m.WriteLine(a[i]);
