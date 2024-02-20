442727758468b13ceeac27c87f84d46e269fe615
IvoMandev <ivomandev6@gmail.com>
2024-02-20 08:49:49 +0200

    Zadacha 10 chisla v diapazon 1 do 100

---
+++ Zadacha2/Zadacha2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vavedete 10 chisla: ");
+            List<int> a = new List<int>();
+            for (int i = 1; i < 10; i++)
+            { 
+                try
+                {
+                    Console.Write($"Vavedete a{i}: ");
+                    a.Add(int.Parse(Console.ReadLine()));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("ÏNvalid format");
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+}
